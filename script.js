363fc1843af4a7d9061ed5d198f1577588363d8c
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-10-25 17:35:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function changeText() {
+            // Find the p tag by its ID
+            var statusElement = document.getElementById("status");
+            
+            // Change the text content of the p tag
+            statusElement.textContent = "Entered Metaverse";
+            
+            // Display the updated text in an h1 tag
+            var resultElement = document.getElementById("result");
+            resultElement.textContent = "Entered Metaverse";
+        }
